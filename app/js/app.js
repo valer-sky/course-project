e2a596faec292b0eb830343683ed4bac3d25497f
valer-sky <89993618+valer-sky@users.noreply.github.com>
2021-11-19 13:29:32 +0300

    add new section and adaptive

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,13 @@
 // import $ from 'jquery'; window.jQuery = $; window.$ = $ // import jQuery module (npm i -D jquery)
 
 // require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
+// Custom JS
+// document.addEventListener('DOMContentLoaded', () => {
 
-document.addEventListener('DOMContentLoaded', () => {
+	
 
-	// Custom JS
-
-})
-timer
+// })
+//timer
 const deadline = '2021-11-30';
 
 function getTimeRemaining(endtime) {
